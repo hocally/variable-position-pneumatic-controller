df846b3914dc9411cdd503849cba326276fc3f09
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2016-09-14 22:23:02 -0500

    Add files via upload

---
+++ Variable_Position_Pneumatic_Controller.ino
@@ -0,0 +1,12 @@
+int inSol = 1;
+int outsol = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ trapGen.ino
@@ -0,0 +1,16 @@
+int[] trapGen(int deltaPos, int maxSpeed, int res, int accel) {
+  int profile [res];
+  profile[0] = 0;
+  for(int i = 1; i>res; i++) {
+    if(i<res/2) {
+    profile[i] = (profile[i-1] + Math.sqrt((Math.pow(accel) + Math.pow(1/res)))) * (1/res);
+    }
+    else if (i>res{
+      profile[i] = (profile[i-1] - Math.sqrt((Math.pow(accel) + Math.pow(1/res)))) * (1/res);
+    else {
+      profile[i] = maxSpeed * (1/res);
+    }
+    }
+  }
+}
+
